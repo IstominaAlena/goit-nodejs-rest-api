b82a12e64355a34d23410777592107e8153be0d3
Istomina Alena <mynikisalena@gmail.com>
2022-02-23 22:39:42 +0200

    123

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://AlenaIstomina:qc4jNzMGrEfUU0v1@cluster0.hqvqz.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error));
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
