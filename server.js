9ca13d5cd18de9e4ab8bfb87f208d0dbfe48feed
Istomina Alena <mynikisalena@gmail.com>
2022-02-23 23:32:32 +0200

    Add mongoDB settings

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const mongoose = require("mongoose");
 const app = require("./app");
+const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://AlenaIstomina:qc4jNzMGrEfUU0v1@cluster0.hqvqz.mongodb.net/db-contacts?retryWrites=true&w=majority";
+require("dotenv").config();
+
+const { DB_HOST } = process.env;
+// "mongodb+srv://AlenaIstomina:qc4jNzMGrEfUU0v1@cluster0.hqvqz.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000");
+});
 
 mongoose
   .connect(DB_HOST)
   .then(() => console.log("Database connection successful"))
   .catch((error) => console.log(error));
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
